558cdda4ee0ff40989a9c4796524078c296a772f
Chris Osborne <osbornch@oregonstate.edu>
2022-05-23 16:17:14 -0700

    Added CommentService and CommentServiceImpl. Need to fix edit post in Impl file.

--- Project2/src/main/java/com/revature/repositories/CommentRepository.java
+++ Project2/src/main/java/com/revature/repositories/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.revature.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +11,8 @@ import com.revature.models.Comment;
 @Transactional
 public interface CommentRepository extends JpaRepository<Comment, Integer>{
 
+	// custom query to edit a comment
+	@Query(value="Update comment SET comment_body?1 WHERE comment_id=?2", nativeQuery = true)
+	public boolean update(String comment_body, int comment_id);
+	
 }
