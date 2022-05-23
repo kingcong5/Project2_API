640517672e200b8701eaecb9c5192d29fe087ceb
jgalena <jgalena1000@gmail.com>
2022-05-23 16:58:58 -0500

    updated image

---
+++ Project2/src/main/java/com/revature/repositories/FavoriteSongRepository.java
@@ -0,0 +1,12 @@
+package com.revature.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.models.FavoriteSong;
+
+public interface FavoriteSongRepository extends JpaRepository<FavoriteSong, Integer>{
+
+}
