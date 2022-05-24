5455c40bc34058b6c5c5e81f0e0f08439b264fc9
jgalena <jgalena1000@gmail.com>
2022-05-24 16:35:18 -0500

    Update SongController.java

--- Project2/src/main/java/com/revature/controllers/SongController.java
+++ Project2/src/main/java/com/revature/controllers/SongController.java
@@ -29,7 +29,43 @@ import com.revature.models.ClientMessage;
 import com.revature.services.SongService;
 
 
-
+@RestController
+@RequestMapping("/api")
+@Api(value = "SongRestController", description = "REST controller releated to song entities")
 public class SongController {
-
+	
+	@Autowired 
+	private SongService sserv;
+	
+	@ApiOperation(value="Find song by id number", notes="Provide an id to lookup specific song from the API", response = Song.class)
+	@GetMapping(path = "/song")
+	public @ResponseBody Song getById(@RequestParam(value = "id", name = "id") int id) {
+		return sserv.getSongById(id);
+		
+	}
+	
+	@GetMapping("/songs")
+	@ApiOperation(value="find all songs")
+	public @ResponseBody List<Song> getAll(){
+		return sserv.getAllSongs();
+	}
+	
+	@PostMapping("/song")
+	@ApiOperation(value="Create new song entity")
+	public @ResponseBody ClientMessage createSong(@RequestBody Song song) {
+		return sserv.createSong(song) ? CREATION_SUCCESSFUL : CREATION_FAILED;
+	}
+	
+	@PutMapping("/song")
+	@ApiOperation(value="Update song entity")
+	public @ResponseBody ClientMessage updateSong(@RequestBody Song song) {
+		return sserv.updateSong(song) ? UPDATE_SUCCESSFUL : UPDATE_FAILED ;
+		
+	}
+	
+	@DeleteMapping("/song")
+	@ApiOperation(value="Remove song entity")
+	public @ResponseBody ClientMessage deleteSong(@RequestBody Song song) {
+		return sserv.deleteSong(song) ? DELETION_SUCCESSFUL : DELETION_FAILED;
+	}
 }
