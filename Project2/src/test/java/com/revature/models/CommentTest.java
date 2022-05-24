3237d673777dd944c09510d1b706acd2a9d7f2e8
kingcong5 <markusgrier83@gmail.com>
2022-05-24 17:15:45 -0400

    added committest

---
+++ Project2/src/test/java/com/revature/models/CommentTest.java
@@ -0,0 +1,48 @@
+package com.revature.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CommentTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCommentStringIntIntInt() {
+		Comment c = new Comment();
+		assertNull(c.getComment_body());
+		assertNull(c.getUser_id());
+		assertNull(c.getComment_likes());
+		assertNull(c.getPost_id());
+	}
+
+	@Test
+	void testCommentIntStringIntIntInt() {
+		Comment c = new Comment();
+		assertNull(c.getPost_id());
+		assertNull(c.getComment_body());
+		assertNull(c.getUser_id());
+		assertNull(c.getComment_likes());
+		assertNull(c.getPost_id());
+	}
+
+}
