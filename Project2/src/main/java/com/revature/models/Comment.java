1d763e20dd4a0abfea850b91960f06f433bb9e87
Chris Osborne <osbornch@oregonstate.edu>
2022-05-25 07:27:43 -0700

    added back in one to ones in comment model

--- Project2/src/main/java/com/revature/models/Comment.java
+++ Project2/src/main/java/com/revature/models/Comment.java
@@ -36,9 +36,9 @@ public class Comment {
 	value = "test comment_body")
 	private String comment_body;
 	
-//	@OneToOne(cascade = CascadeType.ALL)
-//	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
-//	private int user_id;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
+	private int user_id;
 	
 	@Column(name="comment_likes", unique=false)
 	@ApiModelProperty(name="comment_likes", 
@@ -47,33 +47,40 @@ public class Comment {
 	value = "4")
 	private int comment_likes;
 	
-//	@OneToOne(cascade = CascadeType.ALL)
-//	@JoinColumn(name = "post_id", referencedColumnName = "post_id")
-//	private int post_id;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "post_id", referencedColumnName = "post_id")
+	private int post_id;
 
 	public Comment() {
 		super();
 	}
 
-	public Comment(String comment_body, int comment_likes) {
+	public Comment(String comment_body) {
 		super();
 		this.comment_body = comment_body;
-		this.comment_likes = 0;
-
 	}
 
-	public Comment(int comment_id, String comment_body, int comment_likes) {
+	public Comment(int comment_likes) {
+		super();
+		this.comment_likes = comment_likes;
+	}
+
+	public Comment(int comment_id, String comment_body, int user_id, int comment_likes) {
 		super();
 		this.comment_id = comment_id;
 		this.comment_body = comment_body;
-		this.comment_likes = 0;
-
+		this.user_id = user_id;
+		this.comment_likes = comment_likes;
 	}
 
-	public Comment(int comment_id, String comment_body) {
+	public Comment(int comment_id, String comment_body, int user_id, int comment_likes, int post_id) {
 		super();
+		this.comment_id = comment_id;
 		this.comment_body = comment_body;
+		this.user_id = user_id;
+		this.comment_likes = comment_likes;
+		this.post_id = post_id;
 	}
-	
+
 }
 
