a8b5474c70d050d857118efee8db37c39860ff0b
kingcong5 <markusgrier83@gmail.com>
2022-05-31 02:08:56 -0400

    updated files to be logged by log4j and added log4j.properties file

--- Project2/src/main/java/com/revature/services/PostServiceImpl.java
+++ Project2/src/main/java/com/revature/services/PostServiceImpl.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,8 @@ import com.revature.repositories.PostRepository;
 @Service
 @Transactional
 public class PostServiceImpl implements PostService {
+	
+	private static Logger log = Logger.getLogger(PostServiceImpl.class);
 
 	@Autowired
 	private PostRepository comRepo;
@@ -25,6 +28,9 @@ public class PostServiceImpl implements PostService {
 	// create a new post
 	@Override
 	public boolean createPost(Post post) {
+		
+		log.info("In service layer: Creating post: " + post);
+		
 		int pk = comRepo.save(post).getPost_id();
 		return (pk > 0) ? true : false;
 	}
@@ -32,18 +38,27 @@ public class PostServiceImpl implements PostService {
 	// get all post info from post_id
 	@Override
 	public Post getPostById(int post_id) {
+		
+		log.info("In service layer: Getting post by id: " + post_id);
+		
 		return comRepo.findById(post_id);
 	}
 
 	//get all posts
 	@Override
 	public List<Post> getAllPosts() {
+		
+		log.info("In service layer: Getting all posts...");
+		
 		return comRepo.findAll();
 	}
 	
 	//update a post body and likes by post_id
 	@Override
 	public boolean updatePost(Post post) {
+		
+		log.info("In service layer: Updating post: " + post);
+		
 		Post target = comRepo.findById(post.getPost_id());
 		target.setPost_body(post.getPost_body());
 		target.setPost_likes(post.getPost_likes());
@@ -54,6 +69,9 @@ public class PostServiceImpl implements PostService {
 	//update post body by post_id
 	@Override
 	public boolean updatePostBody(Post post) {
+		
+		log.info("In service layer: Updating post body: " + post);
+		
 		Post target = comRepo.findById(post.getPost_id());
 		target.setPost_body(post.getPost_body());
 		return (comRepo.save(target) != null) ? true : false;
@@ -62,6 +80,9 @@ public class PostServiceImpl implements PostService {
 	//delete a post by post_id
 	@Override
 	public boolean deletePost(Post post) {
+		
+		log.info("In service layer: Deleting post: " + post);
+		
 		comRepo.delete(post);
 		return true;
 	}
--- Project2/src/main/java/com/revature/services/UserServiceImpl.java
+++ Project2/src/main/java/com/revature/services/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import java.util.*;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +15,8 @@ import com.revature.repositories.UserRepository;
 @Transactional
 public class UserServiceImpl implements UserService{
 	
+	private static Logger log = Logger.getLogger(UserServiceImpl.class);
+	
 	@Autowired
 	private UserRepository userRepo;
 	
@@ -25,6 +28,9 @@ public class UserServiceImpl implements UserService{
 	// Create new user
 	@Override
 	public boolean createUser(User user) {
+		
+		log.info("In service layer: creating new user: " + user);
+		
 		int pk = userRepo.save(user).getUser_id();
 		return (pk > 0) ? true : false;
 	}
@@ -32,18 +38,27 @@ public class UserServiceImpl implements UserService{
 	// Find user by ID
 	@Override
 	public User getUserById(int user_id) {
+		
+		log.info("In service layer: Finding user by id: " + user_id);
+		
 		return userRepo.findById(user_id);
 	}
 	
 	// Find all users
 	@Override
 	public List<User> getAllUsers() {
+		
+		log.info("In service layer: Getting all users...");
+		
 		return userRepo.findAll();
 	}
 	
 	// Update user
 	@Override
 	public boolean updateUser(User user) {
+		
+		log.info("In service layer: Updating user: " + user);
+		
 		User target = userRepo.findById(user.getUser_id());
 		target.setF_name(user.getF_name());
 		target.setL_name(user.getL_name());
@@ -56,6 +71,9 @@ public class UserServiceImpl implements UserService{
 	// Update user password
 	@Override
 	public boolean updateUserPassword(User user) {
+		
+		log.info("In service layer: Updating user password: " + user);
+		
 		User target = userRepo.findById(user.getUser_id());
 		target.setPassword(user.getPassword());
 		return (userRepo.save(target) != null) ? true : false;
@@ -64,6 +82,9 @@ public class UserServiceImpl implements UserService{
 	// Delete User
 	@Override
 	public boolean deleteUser(User user) {
+		
+		log.info("In service layer: Deleting user: " + user);
+		
 		userRepo.delete(user);
 		return true;
 	}
@@ -71,6 +92,9 @@ public class UserServiceImpl implements UserService{
 	// Login User
 	@Override
 	public User login(String username, String password) {
+		
+		log.info("In service layer: Logging in user: " + username);
+		
 		Optional<User> users = userRepo.findAll().stream().filter(u -> (u.getUsername().equals(username) && u.getPassword().equals(password))).findFirst();
 		return (users.isPresent() ? users.get() : null);
 	}
